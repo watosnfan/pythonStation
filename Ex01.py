47fb1a46972d8a34cf46c22603305288cdc2c747
WatsonFan <30586391+watosnfan@users.noreply.github.com>
2024-07-26 21:44:53 +0800

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+score = int(input("請輸入分數"))
+
+if score >= 60:
+    print("及格")
+else:
+    print("不及格")    
+    
+print("程式執行完畢")    
